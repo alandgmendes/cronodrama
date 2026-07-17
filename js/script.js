ac286caf27f768e7f89da3da79d8f3bc04a190e3
alan douglas mendes <adgmendes@gmail.com>
2026-07-17 12:05:20 -0300

    Versão inicial do projeto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("Cronodrama iniciado.");
+const referencia = new Date(2026, 5, 22); // 22/06/2026
+const hoje = new Date();
+
+referencia.setHours(0, 0, 0, 0);
+hoje.setHours(0, 0, 0, 0);
+
+const msPorDia = 1000 * 60 * 60 * 24;
+const diferencaDias = Math.abs(
+    Math.round((hoje - referencia) / msPorDia)
+);
+
+const nome = document.getElementById("nome");
+const imagem = document.getElementById("imagem");
+
+if (diferencaDias % 2 === 0) {
+    nome.textContent = "Mari Lindinha";
+    imagem.src = "img/par.jpg";
+    imagem.alt = "Maria";
+} else {
+    nome.textContent = "João";
+    imagem.src = "img/impar.jpg";
+    imagem.alt = "Alandouglas";
+}
